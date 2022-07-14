4b36409e8c21c48c6122198ce311620a764d6d04
Ernesto <enriquez906@yahoo.com>
2022-07-14 12:03:36 -0400

    Created new components for the learning site

---
+++ src/components/LearningCenterFeed/LearningCenterFeed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//styling
+import "./LearningCenterFeed.css"
+
+function LearningCenterFeed() {
+  return (
+    <div>LearningCenterFeed</div>
+  )
+}
+
+export default LearningCenterFeed
