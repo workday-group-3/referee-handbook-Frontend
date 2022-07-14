cf65e2101d836ab05749ed99e380b9373249aa78
travisnava <trn5106@psu.edu>
2022-07-13 23:20:22 -0400

    Installed dependency for MUI icons, created skeleton page for login page, no functionality yet.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 
 
+//component imports
+import LoginPage from '../LoginPage/LoginPage'
+
 
 export default function App() {
 
   return (
     <div className="app">
-
 
 
     </div>
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,11 +1,59 @@
 import React from 'react'
 import './LoginPage.css'
 
-export default function LoginPage() {
-  return (
-    <div className="login-page">
 
+//MUI imports
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
+//react imports
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react"
+
+export default function LoginPage(props) {
+  return (
+    <Box
+    component="form"
+    sx={{
+      '& .MuiTextField-root': { m: 2, width: '50ch' },
+    }}
+    noValidate
+    autoComplete="off">
+    <div className="login-page">
+        <div className = "login-page-title">
+          Login
+        </div>
+        <div className="login-form-container">
+          <div className="email-input-container">
+            <TextField
+              className="input-field"
+              id="outlined-email-input"
+              label="Email"
+              type="email"
+              />
+          </div>
+          <div className="password-input-container">
+            <TextField
+              className="input-field"
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"/>
+          </div>
+          <div className="submit-login-btn-container">
+            <Button className="submit-btn" variant="contained" size="large" endIcon={<SendIcon/>}>SUBMIT</Button>
+          </div>
+        </div>
+
+
+        {/* add Link tag here to redirect to register page once routes are established in App.jsx */}
+        <div className="register-redirect">
+          <p>Don't have an account? Register here</p>
+        </div>
     </div>
+    </Box>
   )
 }
 
