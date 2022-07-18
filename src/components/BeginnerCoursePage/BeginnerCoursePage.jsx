0f86f2de8f651d6d813f96c2b9d3fb1e7ab993e4
Ernesto <enriquez906@yahoo.com>
2022-07-18 14:34:00 -0400

    Skeleton for beginner course list in order. Field diagram and youtube video funcitonality also work. Still need to do the timeline and fix the styling

--- src/components/BeginnerCoursePage/BeginnerCoursePage.jsx
+++ src/components/BeginnerCoursePage/BeginnerCoursePage.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 //stylign
 import "./BeginnerCoursePage.css"
 
+//routing
+import { useNavigate } from "react-router-dom"
+
 //import context 
 import { useLearningContext } from '../../contexts/learning'
 
@@ -11,18 +14,55 @@ import LearningBanner from '../learningBanner/LearningBanner'
 
 
 function BeginnerCoursePage() {
+    
+    const navigate = useNavigate()
 
-    const { currentCourse } = useLearningContext()
+    const { currentCourse, setcurrentCourse } = useLearningContext()
+    
+    //when the return button is clicked, user is redirected back to the course list page for their chosen sport
+    async function handleReturn() { 
+        
+        navigate("/learning/:sportName")
+    }
 
     return (
-        <div className='beginnner-course-page'>
+        <div className='beginner-course-page'>
             { currentCourse ? <LearningBanner courseName={currentCourse.sport_name}/> : null}
+          
+            <div className='button-section'>
+                <button className="return-button" onClick={handleReturn}>Return</button>
+
+            </div>
+
             <div className='timeline'>
                 <h3>I am a timeline</h3>
             </div>
-            <div className='return-button'></div>
-            <div className='rules-section'></div>
-            <div className='video-and-diagram-section'></div>
+
+            {currentCourse ? 
+            <div>
+                <div className='rules-section'>
+                    <h1>Rules : </h1>
+                    <p>{currentCourse.beginner_rules}</p>
+                </div>
+                <div className='video-and-diagram-section'>
+                    <div className='video'>
+                    <iframe
+                        src={currentCourse.beginner_tutorial_video_url}
+                        width="500"
+                        height="300"
+                        frameborder="0"
+                        allow="autoplay; encrypted-media"
+                        allowfullscreen
+                        title="video"
+                    />{" "}
+                    </div>
+                    <div className='diagram'>
+                        <h1>Diagram</h1>
+                        <img className="diagram-img" src={currentCourse.beginner_field_diagram_url} alt="beginner field diagram" />
+                    </div>
+                </div>
+            </div> : null}
+            
         </div>
     )
 }
