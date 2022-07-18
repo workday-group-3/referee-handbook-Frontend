87d50a8aa0865626b8b2a77583cdd3b950204945
Ernesto <enriquez906@yahoo.com>
2022-07-18 12:26:32 -0400

    Created new files and directory for the beginner course pages

---
+++ src/components/BeginnerCoursePage/BeginnerCoursePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+//stylign
+import "./BeginnerCoursePage.css"
+
+//import context 
+
+
+function BeginnerCoursePage() {
+  return (
+    <div>BeginnerCoursePage</div>
+  )
+}
+
+export default BeginnerCoursePage
