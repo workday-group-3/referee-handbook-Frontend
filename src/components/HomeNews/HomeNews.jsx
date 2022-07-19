743e88b2fc2f25d9068ef8c4961afd1ffdbea3a1
aileen-ji <aileenji@umich.edu>
2022-07-19 18:51:00 -0400

    modified home news component

--- src/components/HomeNews/HomeNews.jsx
+++ src/components/HomeNews/HomeNews.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import "./HomeNews.css"
+import axios from 'axios'
+
+// API-KEY
+const API_KEY = key
 
 function HomeNews() {
+
+    async function getNews(){
+        try{
+            let json = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token='+API_KEY+"&limit=3&search=basketball")
+        }
+    }
+
   return (
     <div className='home-news'>
         <h2 className='section-title'>HomeNews</h2>
     </div>
   )
-}
+} 
 
 export default HomeNews
---
+++ src/components/HomeNewsCard/HomeNewsCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./HomeNewsCard.css"
+
+function HomeNewsCard() {
+  return (
+    <div className='home-news-card'>HomeNewsCard</div>
+  )
+}
+
+export default HomeNewsCard
