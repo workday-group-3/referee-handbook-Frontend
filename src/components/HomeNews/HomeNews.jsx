ad3d2f66b05b8e41dc4c71f6cee767167e584ba6
aileen-ji <aileenji@umich.edu>
2022-07-19 20:02:32 -0400

    set up basic axios request

--- src/components/HomeNews/HomeNews.jsx
+++ src/components/HomeNews/HomeNews.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./HomeNews.css"
 import axios from 'axios'
 
@@ -6,12 +6,21 @@ import axios from 'axios'
 const API_KEY = key
 
 function HomeNews() {
+  const [news, setNews] = useState([])
+  const [error, setError] = useState(null)
 
     async function getNews(){
         try{
-            let json = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token='+API_KEY+"&limit=3&search=basketball")
+          let json = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token='+API_KEY+"&limit=3&search=basketball")
+          setNews(json.data)
+        } catch (error) {
+          setError(error)
         }
     }
+
+    useEffect(() => {
+      getNews()
+    }, [])
 
   return (
     <div className='home-news'>
