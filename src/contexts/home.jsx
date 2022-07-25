b1195f758bccff84cd2b08ada308e93bb5ff62cc
aileen-ji <aileenji@umich.edu>
2022-07-25 13:26:08 -0400

    edited home context with new function to render three most recent games

--- src/contexts/home.jsx
+++ src/contexts/home.jsx
@@ -24,8 +24,10 @@ export const HomeContextProvider = ({ children }) => {
     const [loadingGame, setLoadingGame] = useState(true)
     const [loadingTeam, setLoadingTeam] = useState(true)
     const [loadingStats, setLoadingStats] = useState(true)
+    const [loadingTeamGames, setLoadingTeamGames] = useState(true)
     const [team, setTeam] = useState(null)
     const [stats, setStats] = useState(null)
+    const [teamGames, setTeamGames] = useState([])
 
     const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY
     const SPORTS_API_KEY = import.meta.env.VITE_SPORTS_API_KEY
@@ -209,16 +211,104 @@ export const HomeContextProvider = ({ children }) => {
         setLoadingStats(false)
     }
 
+    // get the three most recent games for that team
+    async function getTeamGames(sportName, teamId){
+        let apiSportString = 'v1.'+sportName
+        let endpoint = "/games"
+        // the api version for soccer is v3, endpoint is fixtures, different from the rest
+        if(currentSport == "soccer"){
+            apiSportString = "v3.football"
+            endpoint = "/fixtures"
+        }
+
+        try{
+            setLoadingTeamGames(true)
+            // conditionally fetches by the league and season of the specific sport
+            let json = await axios.get("https://"+apiSportString+".api-sports.io"+endpoint+"?league="+requestParams[currentSport].league+"&season="+requestParams[currentSport].season+"&team="+teamId, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": apiSportString+".api-sports.io",
+                    "x-rapidapi-key": SPORTS_API_KEY
+                }
+            })
+            console.log(json)
+            // soccer data is formatted differently, have to filter differently
+            if(currentSport === "soccer"){
+                // filter to find the matches that finished/is in progress
+                let filtered_games = json.data.response.filter((item)=>item.fixture.date < new Date().toISOString())
+                let selected_games = []
+
+                // select the 3 most recent games
+                if(selected_games.length >= 3){
+                    selected_games = [filtered_games[filtered_games.length - 1], filtered_games[filtered_games.length - 2], filtered_games[filtered_games.length - 3]]
+                }
+                else{
+                    for(let i = 0; i < filtered_games.length; i++){
+                        selected_games.push(filtered_games[i])
+                    }
+                }
+                
+
+                // reformat the most recent games
+                for(let j = 0; j < selected_games.length; j++){
+                    // change the key "goals" to "scores" to match the others
+                    selected_games[j].scores = selected_games[j].goals
+                    delete selected_games[j].goals
+
+                    // extract date and status variables to match others
+                    selected_games[j].date = selected_games[j].fixture.date
+                    selected_games[j].status = selected_games[j].fixture.status
+                    delete selected_games[j].fixture
+                }
+                
+
+                // set the recent games
+                setTeamGames(selected_games)
+            }
+            // data filtering for all the other sports
+            else{
+                let filtered_games_others = json.data.response.filter((item)=>item.date < new Date().toISOString())
+                let selected_games = []
+
+                // select the 3 most recent games
+                if(selected_games.length >= 3){
+                    selected_games = [filtered_games_others[filtered_games_others.length - 1], filtered_games_others[filtered_games_others.length - 2], filtered_games_others[filtered_games_others.length - 3]]
+                }
+                else{
+                    for(let i = 0; i < filtered_games_others.length; i++){
+                        selected_games.push(filtered_games_others[i])
+                    }
+                }
+                
+                if(currentSport == "basketball" || currentSport == "baseball"){
+                    // basketball and baseball are formatted differently, extract the total scores to match other formatting
+                    for(let j = 0; j < selected_games.length; j++){
+                        selected_games[j].scores.home = selected_games[j].scores.home.total
+                        selected_games[j].scores.away = selected_games[j].scores.away.total
+                    }
+                    
+                }
+                
+                // set the recent games
+                setTeamGames(selected_games)
+            }
+
+        } catch(error) {
+            setError(error)
+        }
+        setLoadingTeamGames(false)
+    }
+
     
 
     // renders different info as the currentSport changes
     useEffect(() => {
         getNews()
         getTeams()
-       getGame()
+        getGame()
     }, [currentSport])
 
-    const homeValue = {currentSport, setCurrentSport, news, loading, getNews, teams, league, game, loadingGame, getTeam, team, loadingTeam, getStats, stats}
+    const homeValue = {currentSport, setCurrentSport, news, loading, getNews, teams, league, game, loadingGame, getTeam, team, loadingTeam, getStats, stats, loadingStats, getTeamGames, teamGames, loadingTeamGames}
 
     return(
         <HomeContext.Provider value = {homeValue}>
