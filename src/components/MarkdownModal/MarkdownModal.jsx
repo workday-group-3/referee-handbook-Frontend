78006313055d7f04ca75c2991a256aacf93b12e5
ernesto905 <enriquez906@yahoo.com>
2022-07-26 12:04:37 -0400

    further styling for popup

--- src/components/MarkdownModal/MarkdownModal.jsx
+++ src/components/MarkdownModal/MarkdownModal.jsx
@@ -10,24 +10,35 @@ function MarkdownModal({open, onClose}) {
     return (
         <>
 
-        <div className='overlay'></div>
+        <div className='overlay' onClick={onClose}></div>
 
         <div className='modal'>
-            <button onClick={onClose}>Close</button>
-            <h1>Markdown cheat-sheet</h1>
 
-            <h3>Text enhancements</h3>
-            <p>
-                {`<b>`} (some text here) {`</b>`} - Bolden <br />
-                {`<em>`} (some text here) {`</em>`} - Italicize <br />
-                {`<u>`}(some text here {`</u>`} - Underline <br />
-                (some text here) {`</br>`} - newline <br />
-            </p>
-            <h3>Titles</h3>
-            <p>
-                {`<h1>`} (some text) {`</h1>`} - Main header <br />
-                {`<h2>`} (sometext) {`</h2>`} - Sub header
-            </p>
+            <button onClick={onClose}>Close</button>
+
+            {/* Modal display text */}
+            <div className='modal-content'>
+                <h1>Markdown cheat-sheet</h1>
+                <br />
+                <h3>Text enhancements</h3>
+                <p>
+                    {`<b>`}  &nbsp; &nbsp;(some text here) {`</b>`} &nbsp;&nbsp;&nbsp;- Bolden <br />
+                    {`<em>`} (some text here) {`</em>`} - Italicize <br />
+                    {`<u>`}  &nbsp;&nbsp;&nbsp;(some text here) {`</u>`} &nbsp;&nbsp;&nbsp;- Underline <br />
+                </p>
+                <br />
+                <h3>Titles</h3>
+                <p>
+                    {`<h1>`} (some text) {`</h1>`} - Main header <br />
+                    {`<h2>`} (sometext) {`</h2>`} - Sub header
+                </p>
+                <br />
+                <h3>Misc.</h3>
+                <p>
+                    {`<img src=’img url’ alt=’alternativetext’ >`} - Add an image <br />
+                    (some text here) {`<br />`} - newline <br />
+                </p>
+            </div>
 
 
         </div>
