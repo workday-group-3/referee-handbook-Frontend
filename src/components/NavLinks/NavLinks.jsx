d9b9a932535b2cdd39ef968e6880ff3e6c525406
aileen-ji <aileenji@umich.edu>
2022-07-27 19:04:28 -0400

    fixed detail page routing, need to fix css, now can see active link

--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import "./NavLinks.css"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 
 
@@ -8,24 +8,24 @@ import { Link } from "react-router-dom"
 import { useAuthContext } from "../../contexts/auth"
 
 
-export default function NavLinks() {
+export default function NavNavLinks() {
 
 
   const { user, handleOnLogout } = useAuthContext()
 
 
   return (
-    <div className="nav-links">
+    <div className="nav-NavLinks">
       <ul>
-        <li><Link to="/" label="Home">Home</Link></li>
-        <li><Link to="/sports" label="sports">Sports Home</Link></li>
-        <li><Link to="/learning" label="learning">Learning Center</Link></li>
-        <li><Link to="/profile" label="profile">Profile</Link></li>
+        <li><NavLink exact to="/" label="Home" activeClassName="active-link">Home</NavLink></li>
+        <li><NavLink exact to="/sports" label="sports" activeClassName="active-link">Sports Home</NavLink></li>
+        <li><NavLink exact to="/learning" label="learning" activeClassName="active-link">Learning Center</NavLink></li>
+        <li><NavLink exact to="/profile" label="profile" activeClassName="active-link">Profile</NavLink></li>
         {/* Logout button moves slightly up when hovered.. need to make it stay in place like other li elements */}
-        {user?.email ? <li className = "logout-li" onClick={handleOnLogout}><Link to="/login">Logout</Link></li> 
+        {user?.email ? <li className = "logout-li" onClick={handleOnLogout}><NavLink exact to="/login" activeClassName="active-link">Logout</NavLink></li> 
         : <span className ="login-register-li">
-            <li><Link to="/login" label="Login">Login</Link></li>
-            <li><Link to="/register" label="Sign up">Sign Up</Link></li>
+            <li><NavLink exact to="/login" label="Login" activeClassName="active-link">Login</NavLink></li>
+            <li><NavLink exact to="/register" label="Sign up" activeClassName="active-link">Sign Up</NavLink></li>
           </span>}
 
       </ul>
