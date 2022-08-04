89b7bbb7f23839355b853440553d653e406c27bd
aileen-ji <aileenji@umich.edu>
2022-08-04 13:29:26 -0400

    home page css changes

--- src/contexts/home.jsx
+++ src/contexts/home.jsx
@@ -13,8 +13,8 @@ const requestParams = {"basketball": {"league": 12, "season": "2021-2022", "leag
                     "rugby": {"league": 44, "season": "2022", "leagueName": "Major League Rugby"}}
 
 export const HomeContextProvider = ({ children }) => {
-    const [currentSport, setCurrentSport] = useState("baseball")
-    const [league, setLeague] = useState("Major League Soccer")
+    const [currentSport, setCurrentSport] = useState("rugby")
+    const [league, setLeague] = useState("Major League Rugby")
 
     const [news, setNews] = useState([])
     const [teams, setTeams] = useState([])
