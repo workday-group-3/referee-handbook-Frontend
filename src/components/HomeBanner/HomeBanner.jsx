379de30b8bd814766ff19f4df192016efdb3b262
aileen-ji <aileenji@umich.edu>
2022-08-05 12:42:02 -0400

    css edits

--- src/components/HomeBanner/HomeBanner.jsx
+++ src/components/HomeBanner/HomeBanner.jsx
@@ -2,16 +2,6 @@ import React from 'react'
 import "./HomeBanner.css"
 import { useHomeContext } from '../../contexts/home'
 
-// contains all the image urls for different sports banners
-const bannerUrls = {
-  "soccer": "https://images.pexels.com/photos/12486370/pexels-photo-12486370.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "basketball": "https://images.pexels.com/photos/1293265/pexels-photo-1293265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "baseball": "https://images.pexels.com/photos/11901406/pexels-photo-11901406.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "hockey": "https://images.pexels.com/photos/8973426/pexels-photo-8973426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "volleyball": "https://upload.wikimedia.org/wikipedia/commons/c/ce/Beach_volleyball_%284701437938%29.jpg",
-  "rugby": "https://images.pexels.com/photos/2207/red-people-outside-sport.jpg"
-}
-
 function HomeBanner() {
 
   const {currentSport} = useHomeContext()
@@ -19,7 +9,6 @@ function HomeBanner() {
   return (
     <div className='home-banner'>
       <h1 className="home-title">{currentSport}</h1>
-        {/* <img className="banner-img" src={bannerUrls[currentSport]} alt="Home page banner"></img> */}
     </div>
   )
 }
